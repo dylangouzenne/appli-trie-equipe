a462f099a14da7e9a21ea6bfc173c17372cdc47f
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-25 16:59:03 +0200

    création page et navbar

---
+++ app.js
@@ -0,0 +1,14 @@
+var list_users = ["Dania","Adeline","Luc","Aurélien","Nathan","Océane","Dylan","Alexandra","Nicolas","Marc","Helder","Marco","Jonathan","Benjamin","Zakia","Max","Houssine"];
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('se').formSelect();
+  });
+*/
